ed6e8e74a1e53b91a47c3808948a87a005d38072
WitsaKrm <143206207+WitsaKrm@users.noreply.github.com>
2024-01-06 22:34:26 +0700

    Updates

--- README.md
+++
@@ -1,4 +0,0 @@
-# React + Vite
-
-npm run dev -- for run vite  ///
-npm run start -- for run db
--- src/hooks/useTodo.js
+++
@@ -1,7 +0,0 @@
-import { useContext } from "react";
-import TodoContext from "../contexts/todoContext";
-
-
-export default function useTodo() {
-  return useContext(TodoContext)
-}
