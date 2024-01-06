c0fae2cb4851916cd33645cf2276a45a2dc09fed
WitsaKrm <143206207+WitsaKrm@users.noreply.github.com>
2024-01-06 21:46:31 +0700

    uapdate package.json

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'Vite01-ToDoList',
   server: {
     port: 5173,
   },
